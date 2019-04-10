be29991b25b3aac6f11e067827e761566bcbecbe
jooker <zhangzheng@ufotosoft.com>
2019-04-10 15:03:00 +0800

    add test9

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-console.log(1231231123123122)
+console.log(1231231123123122344444444)
 
 new Vue({
   render: h => h(App),
