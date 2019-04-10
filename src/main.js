aac92f34d758a81edca8d3dfeab10d67831935fe
jooker <zhangzheng@ufotosoft.com>
2019-04-10 17:13:25 +0800

    add test13

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 console.log(123123123445542)
-console.log(3334)
+console.log(33777777)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
